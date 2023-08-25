b7201f957c45fdf7c2f122acb3b48150a43416ba
MelnikovDenis <melnikovdv14@gmail.com>
2023-08-26 02:00:19 +0500

    Создал проекты Domain и Services. Создал AppDbContext и сущности User, Role, RefreshToken

---
+++ OnlineLibraryAPI/OnlineLibraryAPI.Domain/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineLibraryAPI.Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string Email { get; set; } = null!;
+
+    public Role Role { get; set; } = null!;
+    public List<RefreshToken> RefreshTokens { get; set; } = null!;
+}
--- OnlineLibraryAPI/OnlineLibraryAPI.Presentation/Program.cs
+++ OnlineLibraryAPI/OnlineLibraryAPI.Presentation/Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using OnlineLibraryAPI.Services.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Добавляем сервисы в контейнер.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Узнайте больше о настройке Swagger/OpenAPI на странице https://aka.ms/aspnetcore/swashbuckle.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
